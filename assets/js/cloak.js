a7c2fae76615ff31160564185221e879b2872a28
Quartinal <prehistorical.dinosaurs@outlook.com>
2024-03-04 16:05:21 -0800

    Updated settings

---
+++ assets/js/cloak.js
@@ -0,0 +1,7 @@
+const i = document.querySelector('.heroSettings .cloak input[placeholder="Favicon URL"]');
+const x = document.querySelector('.heroSettings .cloak input[placeholder="Keybinding"]');
+const a = document.querySelector('.heroSettings .cloak i[class="fa-solid fa-arrow-up-right-from-square"]');
+
+/* 
+ * More to this file is coming soon 
+ */
