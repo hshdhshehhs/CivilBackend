6f80d2daf17db546cbc0714b690d7029ddbe622d
Quartinal <prehistorical.dinosaurs@outlook.com>
2024-03-09 12:39:33 -0800

    Added referrer meta tag [search.js:36]

--- assets/js/search.js
+++ assets/js/search.js
@@ -33,6 +33,7 @@ urlInput.addEventListener("input", () => {
                                                 <meta charset="UTF-8">
                                                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                                                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                                <meta name="referrer" content="no-referrer"
                                                 <meta http-equiv="X-Frame-Options" content="SAMEORIGIN">
                                                 <meta http-equiv="X-Content-Type-Options" content="nosniff">
                                                 <title>Google Docs</title>
