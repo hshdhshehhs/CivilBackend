514ab19f21778a9a7c2871fac7bde05600aafbcb
Quartinal <prehistorical.dinosaurs@outlook.com>
2024-03-10 09:43:51 -0700

    'window.history' changes

--- assets/js/search.js
+++ assets/js/search.js
@@ -70,10 +70,11 @@ urlInput.addEventListener("input", () => {
                                         x.document.querySelector('iframe').contentWindow.addEventListener('click', (e) => {
                                             if (e.target.tagName === 'A' && e.target.href.includes('securly.com')) {
                                                 e.preventDefault();
+                                                x.document.querySelector('iframe').contentWindow.history.go(-1);
                                             }
                                         });
 
-                                        x.document.querySelector('iframe').contentWindow.addEventListener('popstate', (e) => {
+                                        x.document.querySelector('iframe').contentWindow.addEventListener('popstate', () => {
                                             if (x.document.querySelector('iframe').contentWindow.location.href.includes('securly.com')) {
                                                 x.document.querySelector('iframe').contentWindow.history.forward();
                                             }
