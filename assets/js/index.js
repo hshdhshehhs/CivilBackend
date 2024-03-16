c7366b21a75ec4189b3ed4e8993186a9f0406d82
Quartinal <prehistorical.dinosaurs@outlook.com>
2024-03-16 09:42:33 -0700

    renamed!!!

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('../worker.js', {
+        navigator.serviceWorker.register('../sw.js', {
             scope: '/'
         });
     });
