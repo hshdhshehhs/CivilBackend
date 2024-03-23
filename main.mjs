56cb8131c77ebe628f64eb0004d29de0edce6070
Quartinal <prehistorical.dinosaurs@outlook.com>
2024-03-23 16:03:46 -0700

    added cloudflare stuff (temporary)

--- main.mjs
+++ main.mjs
@@ -4,70 +4,72 @@ import {
 } from "https://deno.land/x/oak@v13.2.5/mod.ts";
 import {
     createBareServer
-} from 'npm:@tomphttp/bare-server-node';
+} from "https://dev.jspm.io/@tomphttp/bare-server-node/dist/createServer.js";
 import { basicAuth } from "https://deno.land/x/basic_auth@v1.1.1/mod.ts";
 import config from './config.mjs';
 import { join } from 'https://deno.land/std@0.220.1/path/mod.ts';
 import { serveFile } from 'https://deno.land/std@0.220.1/http/file_server.ts';
-const app = new Application();
-const bareServer = createBareServer('/depo/');
-const PORT = Deno.env.get('PORT') || 8080;
+(async () => {
+    const app = new Application();
+    const bareServer = createBareServer('/depo/');
+    const PORT = Deno.env.get('PORT') || 8080;
 
-const router = new Router();
+    const router = new Router();
 
-if (config.challenge) {
-    console.log('Password protection enabled, usernames: ' + Object.keys(config.users));
-    console.log('Passwords: ' + Object.values(config.users));
+    if (config.challenge) {
+        console.log('Password protection enabled, usernames: ' + Object.keys(config.users));
+        console.log('Passwords: ' + Object.values(config.users));
 
-    app.use(
-        basicAuth({
-            users: config.users,
-            challenge: true,
-        })
-    );
-}
-
-if (config.routes !== false) {
-    const routes = [
-        { path: '/home', file: './assets/index.html' },
-        { path: '/settings', file: './assets/settings.html' }
-    ];
-
-    routes.forEach((route) => {
-        router.get(route.path, (ctx) => {
-            serveFile(ctx, join(__dirname, 'assets', route.file));
-        });
-    }); 
-}
-
-app.use(router.routes());
-
-app.addEventListener('listen', () => {
-    console.log(`Running at http://localhost:${PORT}`);
-});
-
-await app.listen(`:${PORT}`);
-
-bareServer.on('request', (req, res) => {
-    if (bareServer.shouldRoute(req)) {
-        bareServer.routeRequest(req, res);
-    } else {
-        app.handle(req, res);
+        app.use(
+            basicAuth({
+                users: config.users,
+                challenge: true,
+            })
+        );
     }
-});
 
-bareServer.on('upgrade', (req, socket, head) => {
-    if (bareServer.shouldRoute(req)) {
-        bareServer.routeUpgrade(req, socket, head);
-    } else {
-        socket.end();
+    if (config.routes !== false) {
+        const routes = [
+            { path: '/home', file: './assets/index.html' },
+            { path: '/settings', file: './assets/settings.html' }
+        ];
+
+        routes.forEach((route) => {
+            router.get(route.path, (ctx) => {
+                serveFile(ctx, join(__dirname, 'assets', route.file));
+            });
+        }); 
     }
-});
 
-bareServer.on('listening', () => {
-    console.log(`Running at bare: http://localhost:${PORT}/depo/`);
-});
+    app.use(router.routes());
 
-bareServer.listen({
-    port: PORT,
-});
+    app.addEventListener('listen', () => {
+        console.log(`Running at http://localhost:${PORT}`);
+    });
+
+    await app.listen(`:${PORT}`);
+
+    bareServer.on('request', (req, res) => {
+        if (bareServer.shouldRoute(req)) {
+            bareServer.routeRequest(req, res);
+        } else {
+            app.handle(req, res);
+        }
+    });
+
+    bareServer.on('upgrade', (req, socket, head) => {
+        if (bareServer.shouldRoute(req)) {
+            bareServer.routeUpgrade(req, socket, head);
+        } else {
+            socket.end();
+        }
+    });
+
+    bareServer.on('listening', () => {
+        console.log(`Running at bare: http://localhost:${PORT}/depo/`);
+    });
+
+    bareServer.listen({
+        port: PORT,
+    });
+})();
