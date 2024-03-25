e502b9978de32e3688fe3ca39de4553e21914521
Quartinal <prehistorical.dinosaurs@outlook.com>
2024-03-25 16:39:37 -0700

    updated!

--- assets/main.mjs
+++ assets/main.mjs
@@ -7,6 +7,7 @@ import {
 } from 'npm:@tomphttp/bare-server-node';
 import { basicAuth } from "https://deno.land/x/basic_auth@v1.1.1/mod.ts";
 import config from './config.mjs';
+import { serveDir } from "https://deno.land/std@0.220.1/http/file_server.ts";
 const app = new Application();
 const bareServer = createBareServer('/depo/');
 const PORT = Deno.env.get('PORT') || 8080;
@@ -28,7 +29,13 @@ if (config.challenge) {
 if (config.routes !== false) {
     const routes = [
         { path: '/', file: 'index.html' },
-        { path: '/settings', file: 'settings.html' }
+        { path: '/settings', file: 'settings.html' },
+        { path: '/main.css', file: 'main.css' },
+        { path: '/js/index.js', file: 'js/index.js' },
+        { path: '/js/search.js', file: 'js/search.js' },
+        { path: '/js/bundle.js', file: 'js/bundle.js' },
+        { path: '/js/config.js', file: 'js/config.js' },
+        { path: '' }
     ];
 
     routes.forEach((route) => {
