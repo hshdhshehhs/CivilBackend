4a0843aae810b5e40e81d0eca59ec724c306718d
Quartinal <prehistorical.dinosaurs@outlook.com>
2024-03-25 17:56:27 -0700

    Updated!!!

--- assets/main.mjs
+++ assets/main.mjs
@@ -28,8 +28,8 @@ if (config.challenge) {
 
 if (config.routes !== false) {
     const routes = [
-        { path: '/', file: 'index.html' },
-        { path: '/settings', file: 'settings.html' }
+        { path: '/', file: 'assets/index.html' },
+        { path: '/settings', file: 'assets/settings.html' }
     ];
 
     routes.forEach((route) => {
